45fd36461a541acdf121598be6f73182ee7cbb92
Ariel Sanchez <arii.sanchez123@gmail.com>
2021-04-22 17:25:53 -0300

    endpoints helper

---
+++ src/helpers/endpoints.js
@@ -0,0 +1,8 @@
+const endpoints = {
+  posts: `https://jsonplaceholder.typicode.com/posts`,
+  selectPost(id) {
+    return `https://jsonplaceholder.typicode.com/posts/${id}`;
+  }
+};
+
+export default endpoints;
