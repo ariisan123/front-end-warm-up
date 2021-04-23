d75e10f8ccfd150c96cb5f2879ecfdd58e0353e9
Ariel Sanchez <arii.sanchez123@gmail.com>
2021-04-23 19:28:52 -0300

    create post button created

--- src/components/Navbar/CreateButton.js
+++ src/components/Navbar/CreateButton.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button } from 'react-bootstrap';
+import NewPostModal from '../NewPostModal/NewPostModal';
 
 const CreateButton = () => {
-  return <div></div>;
+  const [modal, setModal] = useState(false);
+  const handleModal = () => {
+    setModal(!modal);
+  };
+  return (
+    <React.Fragment>
+      <Button variant="warning" className="m-1" onClick={handleModal}>
+        New Post
+        <i className="fas fa-edit ml-1"></i>
+      </Button>
+
+      <NewPostModal show={modal} setModal={handleModal} />
+    </React.Fragment>
+  );
 };
 
 export default CreateButton;
